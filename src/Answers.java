5c41582eddd19ed8c6297fafdca89d0008fec386
Timothy Geary <tdgeary@gmail.com>
2017-05-30 03:48:34 -0400

    Removed the answer class, integrated into Question.java instead

--- src/Answers.java
+++
@@ -1,6 +0,0 @@
-/**
- * Represents the answers for each question and extract them in the constructor
- * which takes in the file to find them in or queries for user input
- */
-public class Answers{
-}
