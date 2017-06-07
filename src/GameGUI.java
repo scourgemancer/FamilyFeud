2fb369aa967d608f8db5291df7255fd1429ba184
Timothy Geary <tdgeary@gmail.com>
2017-06-07 01:40:06 -0400

    Questions now disregard case when checking an answer

--- src/GameGUI.java
+++ src/GameGUI.java
@@ -1,5 +1,20 @@
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import java.io.File;
+
 /**
  * The graphical view for the game and starting point of the program
  */
 public class GameGUI{
+	Polls polls = new Polls("questions.txt");
+
+	private void playAudio(String filename){
+		Media hit = new Media("src\\resources\\" + filename);
+		MediaPlayer mediaPlayer = new MediaPlayer(hit);
+		mediaPlayer.play();
+	}
+
+	public static void main(String[] args){
+	}
 }
