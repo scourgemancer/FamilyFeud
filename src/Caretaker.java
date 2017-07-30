b5a6e5e121f1c98b767a501fe6aeeb33ddf5e35a
Timothy Geary <tdgeary@gmail.com>
2017-07-29 22:43:53 -0400

    Begun adding the undo/redo feature to the program

---
+++ src/Caretaker.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+/**
+ * Makes mementos for the game as it progresses and keeps track of their order
+ */
+public class Caretaker{
+    private Stack<Memento> undos;
+    private Stack<Memento> redos;
+
+    private boolean canRedo;
+
+    public void save(){
+        //todo - implement
+    }
+
+    public void undo(){
+        //todo - implement
+    }
+
+    public void redo(){
+        //todo - implement
+    }
+}
---
+++ src/Memento.java
@@ -0,0 +1,12 @@
+/**
+ * A saved instance of the game that holds all of the relevant data and knows how to reset it all
+ */
+public class Memento{
+    Memento(GameGUI gui){
+        //todo - implement once everything else is done
+    }
+
+    public void reinstate(){
+        //todo - implement
+    }
+}
