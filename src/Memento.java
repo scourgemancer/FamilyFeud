706da291601805e09f6fa4c53ed406da3a82c668
Timothy Geary <tdgeary@gmail.com>
2017-08-01 09:07:26 -0400

    Memento saves the game state

--- src/Memento.java
+++ src/Memento.java
@@ -2,16 +2,24 @@
  * A saved instance of the game that holds all of the relevant data and knows how to reset it all
  */
 public class Memento{
+    int questionIndex;
+    int savedNumStrikes;
+    int savedMultiplier;
+    int leftTeamPoints;
+    int rightTeamPoints;
+    int unallocatedPoints;
+    boolean[] isHidden;
+
     Memento(GameGUI gui){
-        /**
-         * Things to save:
-         * todo - Each tile being revealed/hidden
-         * todo - The current question
-         * todo - The number of strikes
-         * todo - The multiplier
-         * todo - Each team's points
-         * todo - Unallocated points
-         */
+        questionIndex = gui.currentQuestion;
+        savedNumStrikes = gui.numWrong;
+        savedMultiplier = gui.multiplier;
+        leftTeamPoints = gui.leftTeam;
+        rightTeamPoints = gui.rightTeam;
+        unallocatedPoints = gui.currentPoints;
+        isHidden = new boolean[gui.answerTiles.size()];
+        for(int i=0; i < isHidden.length; i++)
+            isHidden[i] = gui.answerTiles.get(i).hidden;
     }
 
     public void reinstate(){
